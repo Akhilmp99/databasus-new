0e10355efd99f145166f0432719cd8dcd17566e3
AKHIL M P <74479636+Akhilmp99@users.noreply.github.com>
2026-02-23 14:41:13 +0530

    Change link href to empty in MainScreenComponent
    Updated link in MainScreenComponent to an empty href.

--- frontend/src/widgets/main/MainScreenComponent.tsx
+++ frontend/src/widgets/main/MainScreenComponent.tsx
@@ -196,7 +196,7 @@ export const MainScreenComponent = () => {
     <div style={{ height: screenHeight }} className="bg-[#f5f5f5] p-2 md:p-3 dark:bg-gray-900">
       <div className="mb-2 flex h-[50px] items-center rounded bg-white px-2 py-2 shadow md:mb-3 md:h-[60px] md:p-3 dark:bg-gray-800">
         <div className="flex items-center gap-2 hover:opacity-80 md:gap-3">
-          <a href="https://databasus.com" target="_blank" rel="noreferrer">
+          <a href="" target="_blank" rel="noreferrer">
             <img className="h-[30px] w-[30px] p-1 md:h-[40px] md:w-[40px]" src="/logo.svg" />
           </a>
         </div>
